fdc6de972a7e5100b46dbc70111b1572a60edc9e
Nicholas Sullivan-Friedman <nsullivanfriedman@tamdistrict.org>
2023-10-02 21:00:30 +0000

    add user input

--- NumberRiddle.java
+++ NumberRiddle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class NumberRiddle {
     public static void main(String[] args) {
         int positiveInteger = 100;
@@ -13,6 +15,19 @@ public class NumberRiddle {
         ExecuteNumberRiddle(one);
         ExecuteNumberRiddle(positiveDouble); // https://0.30000000000000004.com/
         ExecuteNumberRiddle(negativeDouble);
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number or q to quit: ");
+            String userInput = sc.nextLine();
+            if (userInput.equals("q"))
+                break;
+            try {
+                ExecuteNumberRiddle(Double.parseDouble(userInput));
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid input.");
+            }
+        }
 
     }
 
